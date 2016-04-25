8c7efcd78a86a1ba1b8cf87e0031f637afabc7c4
Md. Rana Mahmud <ranamd0@gmail.com>
2016-04-25 12:16:58 +0600

    Finish user signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  .
-  .
-  .
-  test "valid signup information" do
+  # test "the truth" do
+  #   assert true
+  # end
+ test "valid signup information" do
     get signup_path
     assert_difference 'User.count', 1 do
       post_via_redirect users_path, user: { name:  "Example User",
